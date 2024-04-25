e35eb37df654b357730d7eb3ba5648a39fe3f68a
The Anh <phamtrinhtheanh@gmail.com>
2024-04-25 10:31:18 +0700

    Add all lib

---
+++ doctor/assets/inc/sidebar.php
@@ -0,0 +1,195 @@
+<div class="left-side-menu" style="font-family:Nunito,sans-serif;">
+
+    <div class="slimscroll-menu">
+
+        <div id="sidebar-menu">
+
+            <ul class="metismenu" id="side-menu">
+
+            
+
+                <li>
+                    <a href="index.php">
+                        <i class="fe-airplay"></i>
+                        <span> Dashboard </span>
+                    </a>
+
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="fab fa-accessible-icon "></i>
+                        <span> Patients </span>
+                        <span class="left-t"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" style="fill:rgba(0,0,0,.5);transform: ;msFilter:;">
+                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z">
+                                </path>
+                            </svg></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_doc_register_patient.php">Register Patient</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_view_patients.php">View Patients</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_manage_patient.php">Manage Patients</a>
+                        </li>
+                        <hr>
+                        <li>
+                            <a href="his_doc_discharge_patient.php">Discharge Patients</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_patient_transfer.php">Patient Transfers</a>
+                        </li>
+                    </ul>
+                </li>
+
+
+
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="mdi mdi-pill"></i>
+                        <span> Pharmacy </span>
+                        <span class="left-t"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" style="fill:rgba(0,0,0,.5);transform: ;msFilter:;">
+                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z">
+                                </path>
+                            </svg></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_doc_add_pharm_cat.php">Add Pharm Category</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_view_pharm_cat.php">View Pharm Category</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_manage_pharm_cat.php">Manage Pharm Category</a>
+                        </li>
+                        <hr>
+                        <li>
+                            <a href="his_doc_add_pharmaceuticals.php">Add Pharmaceuticals</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_view_pharmaceuticals.php">View Pharmaceuticals</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_manage_pharmaceuticals.php">Manage Pharmaceuticals</a>
+                        </li>
+                        <hr>
+                        <li>
+                            <a href="his_doc_add_presc.php">Add Prescriptions</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_view_presc.php">View Prescriptions</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_manage_presc.php">Manage Prescriptions</a>
+                        </li>
+                    </ul>
+                </li>
+
+
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class=" fas fa-funnel-dollar "></i>
+                        <span> Inventory </span>
+                        <span class="left-t"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" style="fill:rgba(0,0,0,.5);transform: ;msFilter:;">
+                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z">
+                                </path>
+                            </svg></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+
+                        <li>
+                            <a href="his_doc_pharm_inventory.php">Pharmaceuticals</a>
+                        </li>
+
+                        <li>
+                            <a href="his_doc_equipments_inventory.php">Assets</a>
+                        </li>
+
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="mdi mdi-flask"></i>
+                        <span> Laboratory </span>
+
+                        <span class="left-t"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" style="fill:rgba(0,0,0,.5);transform: ;msFilter:;">
+                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z">
+                                </path>
+                            </svg></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_doc_patient_lab_test.php">Patient Lab Tests</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_patient_lab_result.php">Patient Lab Results</a>
+                        </li>
+                        <li>
+                            <a href="his_doc_patient_lab_vitals.php">Patient Vitals</a>
+                        </li>
+
+                        <li>
+                            <a href="his_doc_lab_report.php">Lab Reports</a>
+                        </li>
+                        <hr>
+
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="mdi mdi-cash-refund "></i>
+                        <span> Payrolls </span>
+                        <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                style="fill:rgba(0,0,0,.5);transform: ;msFilter:;">
+                                <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z">
+                                </path>
+                            </svg></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+
+                        <li>
+                            <a href="his_doc_view_payrolls.php">My Payrolls</a>
+                        </li>
+                    </ul>
+                </li>
+
+
+
+            </ul>
+
+        </div>
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
+<style>
+    li a span {
+        font-family: "Nunito", sans-serif !important;
+    }
+
+
+    .left-side-menu .metismenu>li>a {
+        position: relative;
+    }
+
+    .left-t {
+        position: absolute;
+        right: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+</style>
